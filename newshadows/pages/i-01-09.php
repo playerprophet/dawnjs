1733fb48d1a1e801805e484d1de16d9183447538
Christian DeWolf <christian@wolfbutler.com>
2016-06-11 11:49:32 -0300

    Game placeholders

---
+++ newshadows/pages/i-01-09.php
@@ -0,0 +1,10 @@
+<div class="chapter">
+
+  <div id="beargame"><strong>The game will return in a few days!</strong></div>
+
+  <p>&gt;Move with arrow keys or WASD</p>
+  <p>&gt;Select with Space or X</p>
+  <p>&gt;Cancel with ESC or Z</p>
+
+</div>
+
