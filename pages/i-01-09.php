11442f4576b47e24ecb370919a7f96c6a76d140a
Christian DeWolf <christian@wolfbutler.com>
2016-06-19 23:23:35 -0300

    Insert bear game

---
+++ pages/i-01-09.php
@@ -0,0 +1,16 @@
+<div class="chapter">
+
+  <div id="enchant-stage"></div>
+
+  <p>&gt;Move with arrow keys or WASD</p>
+  <p>&gt;Select with Space or X</p>
+  <p>&gt;Cancel with ESC or Z</p>
+
+</div>
+
+<script type="application/javascript" src="game2/enchant.js"></script>
+<script type="application/javascript" src="game2/ui.enchant.js"></script>
+<script type="application/javascript" src="game2/classes.js"></script>
+<script type="application/javascript" src="game2/script.js"></script>
+
+<script>startBearGame()</script>
